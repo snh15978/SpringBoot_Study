f0f94b08826e137401797a06de6227f9864d3054
DESKTOP-VF6KKVO\Good <snh15978@gmail.com>
2018-08-01 23:32:13 +0900

    initial commit

---
+++ src/main/java/net/springboot/MySpringbootApplication.java
@@ -0,0 +1,12 @@
+package net.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootApplication.class, args);
+	}
+}
